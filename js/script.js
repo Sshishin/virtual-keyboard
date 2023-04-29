9462020adc9136a1bb2c76c3e8daf887b2572439
Sergey Shishin <flyers32rus@gmail.com>
2023-04-30 00:19:52 +0300

    feat: add html structure

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
+const body = document.querySelector('body');
 
+const html = `
+    <main class="container">
+        <div class="keyboard">
+            <section class="keyboard-input">
+                <input class="keyboard-input__area"/>
+            </section>
+            <section class="keyboard-desk">
+                <div class="keyboard-desk__shape"></div>
+            </section>
+        </div>
+    </main>
+`;
+
+body.insertAdjacentHTML('afterbegin', html);
