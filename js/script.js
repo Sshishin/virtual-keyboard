e4c6a9a0a2fb0605d6a5af36de2f321dbd2c5297
Sergey Shishin <flyers32rus@gmail.com>
2023-05-01 13:03:00 +0300

    feat: add change lang

--- js/script.js
+++ js/script.js
@@ -2,12 +2,21 @@ const body = document.querySelector('body');
 
 // TODO: Использовать Modules 
 
-const html = `
+const inputArea = `
     <main class="container">
         <div class="keyboard">
             <section class="keyboard-input">
                 <textarea class="keyboard-input__area"></textarea>
             </section>
+        </div>
+    </main>
+`;
+
+body.insertAdjacentHTML('afterbegin', inputArea);
+
+const keyboardInput = document.querySelector('.keyboard-input');
+
+const htmlEN = `
             <section class="keyboard-desk">
                 <div class="keyboard-desk__shape">
                     <button id="192" data="Backquote" class="tilda">~</button>
@@ -76,30 +85,134 @@ const html = `
                     <button id="39" data="ArrowRight" class="arrow-right">ArrowRight</button>
                 </div>
             </section>
-        </div>
-    </main>
 `;
 
-body.insertAdjacentHTML('afterbegin', html);
+const htmlRU = `
+            <section class="keyboard-desk">
+                <div class="keyboard-desk__shape">
+                    <button id="192" data="Backquote" class="tilda">~</button>
+                    <button id="49" data="Digit1" class="one">1</button>
+                    <button id="50" data="Digit2" class="two">2</button>
+                    <button id="51" data="Digit3" class="three">3</button>
+                    <button id="52" data="Digit4" class="four">4</button>
+                    <button id="53" data="Digit5" class="five">5</button>
+                    <button id="54" data="Digit6" class="six">6</button>
+                    <button id="55" data="Digit7" class="seven">7</button>
+                    <button id="56" data="Digit8" class="eight">8</button>
+                    <button id="57" data="Digit9" class="nine">9</button>
+                    <button id="48" data="Digit0" class="null">0</button>
+                    <button id="189" data="Minus" class="minus">-</button>
+                    <button id="187" data="Equal" class="equal">=</button>
+                    <button id="8" data="Backspace" class="backspace">Backspace</button>
+                    <button id="9" data="Tab" class="tab">Tab</button>
+                    <button id="81" data="KeyQ" class="q">Й</button>
+                    <button id="87" data="KeyW" class="w">Ц</button>
+                    <button id="69" data="KeyE" class="e">У</button>
+                    <button id="82" data="KeyR" class="r">К</button>
+                    <button id="84" data="KeyT" class="t">Е</button>
+                    <button id="89" data="KeyY" class="y">Н</button>
+                    <button id="85" data="KeyU" class="u">Г</button>
+                    <button id="73" data="KeyI" class="i">Ш</button>
+                    <button id="79" data="KeyO" class="o">Щ</button>
+                    <button id="80" data="KeyP" class="p">З</button>
+                    <button id="219" data="BracketLeft" class="bracket-left">З</button>
+                    <button id="221" data="BracketRight" class="bracket-right">Х</button>
+                    <button id="220" data="Backslash" class="backslash">Ъ</button>
+                    <button id="8" data="Delete" class="del">Del</button>
+                    <button id="20" data="CapsLock" class="caps">Caps Lock</button>
+                    <button id="65" data="KeyA" class="a">Ф</button>
+                    <button id="83" data="KeyS" class="s">Ы</button>
+                    <button id="68" data="KeyD" class="d">В</button>
+                    <button id="70" data="KeyF" class="f">А</button>
+                    <button id="71" data="KeyG" class="g">П</button>
+                    <button id="72" data="KeyH" class="h">Р</button>
+                    <button id="74" data="KeyJ" class="j">О</button>
+                    <button id="75" data="KeyK" class="k">Л</button>
+                    <button id="76" data="KeyL" class="l">Д</button>
+                    <button id="186" data="Semicolon" class="semicolon">Ж</button>
+                    <button id="222" data="Quote" class="quote">Э</button>
+                    <button id="13" data="Enter" class="enter">Enter</button>
+                    <button id="16" data="ShiftLeft" class="shift-left">Shift</button>
+                    <button id="90" data="KeyZ" class="z">Я</button>
+                    <button id="88" data="KeyX" class="x">Ч</button>
+                    <button id="192" data="KeyC" class="c">С</button>
+                    <button id="67" data="KeyV" class="v">М</button>
+                    <button id="86" data="KeyB" class="b">И</button>
+                    <button id="78" data="KeyN" class="n">Т</button>
+                    <button id="77" data="KeyM" class="m">Ь</button>
+                    <button id="188" data="Period" class="period">Б</button>
+                    <button id="190" data="Comma" class="comma">Ю</button>
+                    <button id="191" data="Slash" class="slash">/</button>
+                    <button id="38" data="ArrowUp" class="arrow-up">ArrowUp</button>
+                    <button id="16" data="ShiftRight" class="shift-right">Shift</button>
+                    <button id="17" data="ControlLeft" class="ctrl-left">Ctrl</button>
+                    <button id="91" data="MetaLeft" class="win">Win</button>
+                    <button id="18" data="AltLeft" class="alt-left">Alt</button>
+                    <button id="32" data="Space" class="space">Space</button>
+                    <button id="18" data="AltRight" class="alt-right">Alt</button>
+                    <button id="17" data="ControlRight" class="ctrl-right">Ctrl</button>
+                    <button id="37" data="ArrowLeft" class="arrow-left">ArrowLeft</button>
+                    <button id="40" data="ArrowDown" class="arrow-down">ArrowDown</button>
+                    <button id="39" data="ArrowRight" class="arrow-right">ArrowRight</button>
+                </div>
+            </section>
 
-const keyboardParent = document.querySelector('.keyboard-desk__shape');
+`;
 
-keyboardParent.addEventListener('mousedown', (e) => {
-    const currentMouse = document.querySelector(`.${e.target.className}`);
-    currentMouse.classList.add('active');
-    const input = document.querySelector('.keyboard-input__area');
-    input.value += currentMouse.innerHTML;
-});
+function changeLang() {
+    if (localStorage.getItem('lang') == 'ru') {
+        const lang = document.querySelector('.keyboard-desk');
+        if (lang) {
+            lang.remove();
+            keyboardInput.insertAdjacentHTML('afterend', htmlRU);
+        } else {
+            keyboardInput.insertAdjacentHTML('afterend', htmlRU);
+        }
 
-keyboardParent.addEventListener('mouseup', (e) => {
-    const currentMouse = document.getElementById(`${e.target.id}`);
-    currentMouse.classList.remove('active');
-});
+    } else {
+        const lang = document.querySelector('.keyboard-desk');
+        if (lang) {
+            lang.remove();
+            keyboardInput.insertAdjacentHTML('afterend', htmlEN);
+        } else {
+            keyboardInput.insertAdjacentHTML('afterend', htmlEN);
+        }
+    }
+
+    mouseListeners();
+}
+changeLang();
+
+function mouseListeners() {
+    const keyboardParent = document.querySelector('.keyboard-desk__shape');
+
+    keyboardParent.addEventListener('mousedown', (e) => {
+        const currentMouse = document.querySelector(`.${e.target.className}`);
+        currentMouse.classList.add('active');
+        const input = document.querySelector('.keyboard-input__area');
+        input.value += currentMouse.innerHTML;
+    });
+
+    keyboardParent.addEventListener('mouseup', (e) => {
+        const currentMouse = document.getElementById(`${e.target.id}`);
+        currentMouse.classList.remove('active');
+    });
+}
 
 document.addEventListener('keydown', (e) => {
-    e.preventDefault();
+
+    if (e.altKey && e.code === "ShiftLeft") {
+        e.preventDefault();
+        if (localStorage.getItem('lang') == 'ru') {
+            localStorage.setItem('lang', 'en');
+            changeLang();
+        } else {
+            localStorage.setItem('lang', 'ru');
+            changeLang();
+        }
+    }
+
     const current = document.querySelector(`[data="${e.code}"]`);
-    console.log(current.innerHTML);
     current.classList.add('active');
     const input = document.querySelector('.keyboard-input__area');
     input.value += current.innerHTML;
@@ -108,6 +221,5 @@ document.addEventListener('keydown', (e) => {
 document.addEventListener('keyup', (e) => {
     e.preventDefault();
     const current = document.querySelector(`[data="${e.code}"]`);
-    console.log(current);
     current.classList.remove('active');
 });
